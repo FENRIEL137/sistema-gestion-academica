7ec87d7a64bb1c80c1ffc1603f482bcd60263b47
Gabriel Fidel Thokokary Jaldin <thokokaryjaldingabriel@gmail.com>
2026-06-23 21:17:25 +0200

    Proyecto limpio - solo PostgreSQL

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,8 +6,7 @@ namespace SistemaGestionAcademica.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
@@ -27,18 +26,12 @@ namespace SistemaGestionAcademica.Data
         {
             base.OnModelCreating(builder);
 
-            // Configuracion de ApplicationUser
-            builder.Entity<ApplicationUser>(entity =>
-            {
-                entity.Property(u => u.NombreCompleto).IsRequired().HasMaxLength(200);
-            });
-
-            // Configuracion de indices unicos
+            // Indices unicos
             builder.Entity<Estudiante>().HasIndex(e => e.CI).IsUnique();
             builder.Entity<Profesor>().HasIndex(p => p.CI).IsUnique();
             builder.Entity<Empleado>().HasIndex(e => e.CI).IsUnique();
 
-            // Configuracion de relaciones
+            // Relaciones
             builder.Entity<Inscripcion>()
                 .HasOne(i => i.Estudiante)
                 .WithMany(e => e.Inscripciones)
@@ -68,42 +61,6 @@ namespace SistemaGestionAcademica.Data
                 .WithMany(m => m.Actividades)
                 .HasForeignKey(a => a.MateriaId)
                 .OnDelete(DeleteBehavior.Cascade);
-
-            builder.Entity<Materia>()
-                .HasOne(m => m.Profesor)
-                .WithMany(p => p.Materias)
-                .HasForeignKey(m => m.ProfesorId)
-                .OnDelete(DeleteBehavior.SetNull);
-
-            builder.Entity<Materia>()
-                .HasOne(m => m.Aula)
-                .WithMany(a => a.Materias)
-                .HasForeignKey(m => m.AulaId)
-                .OnDelete(DeleteBehavior.SetNull);
-
-            builder.Entity<Materia>()
-                .HasOne(m => m.Horario)
-                .WithMany(h => h.Materias)
-                .HasForeignKey(m => m.HorarioId)
-                .OnDelete(DeleteBehavior.SetNull);
-
-            builder.Entity<Pago>()
-                .HasOne(p => p.Inscripcion)
-                .WithMany(i => i.Pagos)
-                .HasForeignKey(p => p.InscripcionId)
-                .OnDelete(DeleteBehavior.SetNull);
-
-            builder.Entity<Pago>()
-                .HasOne(p => p.Estudiante)
-                .WithMany()
-                .HasForeignKey(p => p.EstudianteId)
-                .OnDelete(DeleteBehavior.SetNull);
-
-            // =============================================
-            // DATOS INICIALES (Seed Data) - CORREGIDO
-            // =============================================
-            // NOTA: NO usamos IDs fijos, dejamos que la BD los genere
-            // Los datos iniciales se insertan en DataInitializer.cs
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Npgsql;
 using SistemaGestionAcademica.Data;
 using SistemaGestionAcademica.Models.Entities;
 using SistemaGestionAcademica.Repositories.Interfaces;
 using SistemaGestionAcademica.Repositories;
 using SistemaGestionAcademica.Services;
 using SistemaGestionAcademica.Services.Interfaces;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,40 +16,32 @@ builder.WebHost.UseUrls($"http://*:{port}");
 builder.Services.AddControllersWithViews();
 
 // =============================================
-// BASE DE DATOS - POSTGRESQL PARA RENDER
+// SOLO POSTGRESQL
 // =============================================
-var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL")
-    ?? builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? "Host=localhost;Database=siga;Username=postgres;Password=postgres";
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+if (!string.IsNullOrEmpty(databaseUrl))
 {
-    if (connectionString.StartsWith("postgres://") || connectionString.StartsWith("postgresql://"))
-    {
-        // Convertir URL de Render a formato de conexion
-        var uri = new Uri(connectionString);
-        var userInfo = uri.UserInfo.Split(':');
-        var host = uri.Host;
-        var portDb = uri.Port > 0 ? uri.Port : 5432;
-        var database = uri.AbsolutePath.TrimStart('/');
-        var username = userInfo[0];
-        var password = userInfo.Length > 1 ? userInfo[1] : "";
+    // Render proporciona DATABASE_URL
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    var host = uri.Host;
+    var portDb = uri.Port;
+    var database = uri.AbsolutePath.TrimStart('/');
+    var username = userInfo[0];
+    var password = userInfo.Length > 1 ? userInfo[1] : "";
 
-        var npgsqlConnString = $"Host={host};Port={portDb};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true;Include Error Detail=true";
+    var connString = $"Host={host};Port={portDb};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
 
-        options.UseNpgsql(npgsqlConnString);
-    }
-    else if (connectionString.Contains("Data Source=") || connectionString.Contains(".db"))
-    {
-        // SQLite (desarrollo local alternativo)
-        options.UseSqlite(connectionString);
-    }
-    else
-    {
-        // SQL Server (desarrollo local)
-        options.UseSqlServer(connectionString);
-    }
-});
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql(connString));
+}
+else
+{
+    // Desarrollo local
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql("Host=localhost;Database=siga;Username=postgres;Password=postgres"));
+}
 
 // Identity
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
@@ -67,26 +59,12 @@ builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromHours(8);
     options.LoginPath = "/Account/Login";
     options.AccessDeniedPath = "/Account/AccessDenied";
-    options.SlidingExpiration = true;
 });
 
 // Repositorios
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddScoped<IEstudianteRepository, EstudianteRepository>();
-builder.Services.AddScoped<IProfesorRepository, ProfesorRepository>();
-builder.Services.AddScoped<IEmpleadoRepository, EmpleadoRepository>();
-builder.Services.AddScoped<IMateriaRepository, MateriaRepository>();
-builder.Services.AddScoped<IAulaRepository, AulaRepository>();
-builder.Services.AddScoped<IHorarioRepository, HorarioRepository>();
-builder.Services.AddScoped<IInscripcionRepository, InscripcionRepository>();
-builder.Services.AddScoped<IPagoRepository, PagoRepository>();
-builder.Services.AddScoped<IActividadRepository, ActividadRepository>();
-builder.Services.AddScoped<INotaRepository, NotaRepository>();
-builder.Services.AddScoped<IConfiguracionInstitucionalRepository, ConfiguracionInstitucionalRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEstudianteService, EstudianteService>();
 builder.Services.AddScoped<IProfesorService, ProfesorService>();
@@ -120,26 +98,12 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// Crear tablas y datos iniciales
+// Inicializar
 using (var scope = app.Services.CreateScope())
 {
-    var services = scope.ServiceProvider;
-    var logger = services.GetRequiredService<ILogger<Program>>();
-
-    try
-    {
-        var context = services.GetRequiredService<ApplicationDbContext>();
-        logger.LogInformation("Creando base de datos...");
-        await context.Database.EnsureCreatedAsync();
-        logger.LogInformation("Base de datos lista.");
-
-        await DataInitializer.InitializeAsync(services);
-        logger.LogInformation("Datos iniciales insertados.");
-    }
-    catch (Exception ex)
-    {
-        logger.LogError(ex, "Error al inicializar: " + ex.Message);
-    }
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await context.Database.EnsureCreatedAsync();
+    await DataInitializer.InitializeAsync(scope.ServiceProvider);
 }
 
 app.Run();
