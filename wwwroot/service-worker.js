efbe4587ddf5d7e2d8b64819eda624979f295a15
Gabriel Fidel Thokokary Jaldin <thokokaryjaldingabriel@gmail.com>
2026-06-29 18:03:51 +0200

    Cambiar el almacenamiento de cache

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -1,30 +1,55 @@
-const CACHE_NAME = 'siga-v1';
+const CACHE_NAME = 'siga-v2';
+
 const urlsToCache = [
     '/',
-    '/Home/Index',
-    '/Account/Login',
     '/css/site.css',
     '/manifest.json'
 ];
 
+// Instalación
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
+
     self.skipWaiting();
 });
 
+// Activación
 self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-        ))
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
     );
+
     self.clients.claim();
 });
 
+// Network First
 self.addEventListener('fetch', event => {
+
+    if (event.request.method !== 'GET')
+        return;
+
     event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
+
+        fetch(event.request)
+            .then(response => {
+
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => cache.put(event.request, responseClone));
+
+                return response;
+            })
+
+            .catch(() => caches.match(event.request))
     );
+
 });
